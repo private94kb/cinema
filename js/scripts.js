209982b143a16a64429a41cb63d9bad05a67a9c3
Katya Borysiuk <borisyukkatia94@gmail.com>
2021-10-12 18:47:19 +0300

    comletion of the project

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+const schemeSvg = document.querySelector(".scheme_svg");
+const totalPriceTag = document.querySelector('.price_total');
+const menuButton = document.querySelector('.m_menu');
+const menu = document.querySelector('.menu');
+  let cost = 200;
+  let totalPrice = 0;
+schemeSvg.addEventListener("click",(event) => {
+    if (!event.target.classList.contains("booked")){
+        event.target.classList.toggle("active");
+        let totalSeats= schemeSvg.querySelectorAll(".active").length;
+        totalPrice = totalSeats * cost;
+        totalPriceTag.textContent = totalPrice;
+    }
+});
+menuButton.addEventListener("click",() =>{
+    console.log('click on menu');
+    menu.classList.toggle('is_open');
+});
+
+
